356cbba763c864a305b7f69167f216c36c410d81
Simon <sschol24@smail.uni-koeln.de>
2023-01-13 12:39:59 +0100

    Add the relevant files up to section 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { ViewbooksComponent } from './viewbooks/viewbooks.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddbookComponent,
+    ViewbooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewbooks/viewbooks.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookstoreService } from '../bookstore.service';
+import { Book } from '../book';
+
+@Component({
+  selector: 'app-viewbooks',
+  templateUrl: './viewbooks.component.html',
+  styleUrls: ['./viewbooks.component.css']
+})
+export class ViewbooksComponent implements OnInit {
+  books: Array<Book> = [];
+
+  constructor(private _service: BookstoreService, private _route: Router) {}
+
+  ngOnInit(): void {
+    this.getbooks();
+  }
+
+  getbooks(){
+    this._service.getBooksFromServer().subscribe(data => this.books = data)
+  }
+
+  RedirectToAddBook(){
+    this._route.navigate(['/addbook'])
+  }
+}
